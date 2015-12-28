a320f6998bd0c16c0d3e35073483a6669fedb159
Lukas Linhart <lukas@apiary.io>
2015-12-28 17:51:57 +0100

    Make build file more readable by extracting magic names to variables

--- build.sh
+++ build.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+SPHINX_IMAGE_NAME="apiaryio/base-sphinx-doc-dev"
+TAG_NAME="apiaryio/example-intersphinx-repo-shared-inventories"
+CONTAINER_NAME="example-intersphinx-repo-shared-inventories-volume"
+
 (
 	cd "./example-standalone-docs/" && \
-	docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs apiaryio/base-sphinx-doc-dev
+	docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs $SPHINX_IMAGE_NAME
 ) && \
 
 cp ./example-standalone-docs/build/html/objects.inv ./shared-invs/inventories/other-project.inv && \
 
 (
 	cd "./shared-invs" && \
-	docker build -t example-intersphinx-repo-shared-inventories . 
+	docker build -t $TAG_NAME .
 ) && \
 
-docker create -v /inventories --name example-intersphinx-repo-shared-inventories example-intersphinx-repo-shared-inventories /bin/true && \
+docker create -v /inventories --name=$CONTAINER_NAME $TAG_NAME /bin/true && \
 
 (
 	cd "./example-intersphinx-project/" && \
-	docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs --volumes-from=example-intersphinx-repo-shared-inventories apiaryio/base-sphinx-doc-dev
+	docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs --volumes-from=$CONTAINER_NAME $SPHINX_IMAGE_NAME
 )
 
 exitcode=$?
 
-docker rm example-intersphinx-repo-shared-inventories
+docker rm $CONTAINER_NAME > /dev/null
 
 exit $exitcode
