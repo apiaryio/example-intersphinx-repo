fb8878d48df93f2fd60d4a3f0b40b4af16b5baba
Lukas Linhart <lukas@apiary.io>
2015-12-28 17:17:50 +0100

    Bash readability cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-( cd "./example-standalone-docs/" && docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs apiaryio/base-sphinx-doc-dev ) && \
+(
+	cd "./example-standalone-docs/" && \
+	docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs apiaryio/base-sphinx-doc-dev
+) && \
+
 cp ./example-standalone-docs/build/html/objects.inv ./shared-invs/inventories/other-project.inv && \
-(cd "./shared-invs" && docker build -t example-intersphinx-repo-shared-inventories . ) && \
+
+(
+	cd "./shared-invs" && \
+	docker build -t example-intersphinx-repo-shared-inventories . 
+) && \
+
 docker create -v /inventories --name example-intersphinx-repo-shared-inventories example-intersphinx-repo-shared-inventories /bin/true && \
-(cd "./example-intersphinx-project/" && docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs --volumes-from=example-intersphinx-repo-shared-inventories apiaryio/base-sphinx-doc-dev )
+
+(
+	cd "./example-intersphinx-project/" && \
+	docker run -ti --rm -v $(pwd):/mnt/docs:rw -w /mnt/docs --volumes-from=example-intersphinx-repo-shared-inventories apiaryio/base-sphinx-doc-dev
+)
+
+exitcode=$?
 
 docker rm example-intersphinx-repo-shared-inventories
+
+exit $exitcode
